975811120b7c5c5e543d475f478b40fbe180142b
chiranjeev <chs.2609@gmail.com>
2014-10-17 23:48:22 +0530

    Adding a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FirstApp::Application.routes.draw do
+  root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
